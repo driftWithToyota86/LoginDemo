a3e4535c5c1c1f7cb657a958bfb43b2d17465249
yuyu <yuyugainst@hotmail.com>
2019-04-18 02:36:10 +0800

    UserInfoServlet serialVersionUID

--- src/top/pushme/servlet/UserInfoServlet.java
+++ src/top/pushme/servlet/UserInfoServlet.java
@@ -10,6 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
 public class UserInfoServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
